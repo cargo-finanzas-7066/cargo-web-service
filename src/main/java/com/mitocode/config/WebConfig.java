ba33010b2c104da0cf73bacbb3eb61c8f7c4773c
jahazielgg <u202319831@upc.edu.pe>
2026-07-01 00:08:20 -0500

    feat(config): add WebConfig for API path prefix configuration

---
+++ src/main/java/com/mitocode/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.mitocode.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public static final String API_PREFIX = "/api/v1";
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix(API_PREFIX, HandlerTypePredicate.forBasePackage("com.mitocode"));
+    }
+}
